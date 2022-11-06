14c065b16b8d050b1c150dc77610296af9c5cef1
Ahmed <morsyahmed402@gmail.com>
2022-11-07 01:21:39 +0200

    todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { editIcon, trashIcon } from "./icons";
 
-function App() {
+const App = () => {
+  const [value, setValue] = useState("");
+  const [listItems, setListItems] = useState([]);
+  const [alert, setAlert] = useState({ show: false, type: "", msg: "" });
+  const [editID, setEditID] = useState(null);
+  const [editing, setEditing] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) {
+      showAlert(true, "danger", "There is no Item");
+    } else if (value && editing) {
+      const editedItemsList = listItems.map((item) => {
+        if (editID === item.id) {
+          setValue(value);
+
+          return { ...item, title: value };
+        }
+        return item;
+      });
+      setListItems(editedItemsList);
+      setValue("");
+      setEditID(null);
+      setEditing(false);
+      showAlert(true, "success", "Item Edited");
+    } else {
+      setListItems([
+        ...listItems,
+        { id: new Date().getTime().toString(), title: value },
+      ]);
+      setValue("");
+      showAlert(true, "success", "Item added");
+    }
+  };
+
+  const removeAll = () => {
+    setListItems([]);
+    showAlert(true, "success", "List is clear");
+  };
+
+  const removeItem = (id, title) => {
+    const newList = listItems.filter((item) => item.id !== id);
+    setListItems(newList);
+    showAlert(true, "danger", `${title} removed`);
+  };
+
+  const editItem = (id) => {
+    const editedItemsList = listItems.map((item) => {
+      if (id === item.id) {
+        setValue(item.title);
+        setEditID(id);
+      }
+      return item;
+    });
+    setListItems(editedItemsList);
+    setEditing(true);
+  };
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      showAlert(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className={alert.type}>{alert.show && <p>{alert.msg}</p>}</div>
+      <form action="" onSubmit={handleSubmit}>
+        <div className="inputSubmit">
+          <input
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            type="text"
+            placeholder="Enter..."
+          />
+          <button className="submitBtn">{editing ? "Edit" : "Submit"}</button>
+        </div>
+        <div>
+          {listItems.length
+            ? listItems.map((item) => (
+                <div className="itemContainer" key={item.id}>
+                  <h4>{item.title}</h4>
+                  <div className="icons">
+                    <div onClick={() => editItem(item.id)} className="editBtn">
+                      {editIcon}
+                    </div>
+
+                    <div
+                      onClick={() => removeItem(item.id, item.title)}
+                      className="trashBtn"
+                    >
+                      {trashIcon}
+                    </div>
+                  </div>
+                </div>
+              ))
+            : null}
+        </div>
+      </form>
+
+      {listItems.length ? (
+        <button onClick={removeAll} className="clearAll">
+          Clear All
+        </button>
+      ) : null}
+    </main>
   );
-}
+};
 
 export default App;
